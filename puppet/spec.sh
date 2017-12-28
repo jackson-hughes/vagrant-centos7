30729d98fe8e548ddb5e815bdf8fc9166deeb5c0
Jackson Mark Hughes <jhughes@Jacksons-MBP.jhcloud.io>
2017-12-28 18:34:42 +0000

    cleaned up files not specific to Vagrantfile and added gitignore for .vagrant directory.

--- puppet/spec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Super simple wrapper for inspec exec with vagrant config
-
-inspec exec $1 -t ssh://vagrant@127.0.0.1:2222 -i /Users/jhughes/vagrant-centos7/puppet/.vagrant/machines/default/virtualbox/private_key
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Vagrantfile for CentOS 7 base image including Puppet agent.
+
+### Puppet agent will be provisioned via shell script always pulling latest binary.
